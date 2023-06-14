535839df93e53cc6a66f231cc7f84dbbaabe27be
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-14 19:09:08 -0300

    solucao

---
+++ Algoritmo17.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	array := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	soma := 0
+
+	for i, valor := range array {
+		if i%2 == 0 {
+			soma += valor
+		}
+	}
+
+	fmt.Println("A soma dos elementos nas posições pares é:", soma)
+}
